92e8879a14cbe4363f49e332226d8c6a1383249b
Arthur-Cstln <104087861+Arthur-Cstln@users.noreply.github.com>
2023-09-25 22:58:20 +0200

    requete fonctionnel a améliorer

--- IHM/index.js
+++ IHM/index.js
@@ -1,14 +1,35 @@
-function showData(RandomNumber){
-    $.ajax({
-        type: "POST",
-        url: "ajax.php",
-        data: {
-            action :"showData",
-            number : RandomNumber
-        },
-        dataType: "djson",
-        success: function (response) {
-            console.log(reponse);
-        }
-    });
+// function showData(RandomNumber){
+//     $.ajax({
+//         type: "POST",
+//         url: "ajax.php",
+//         data: {
+//             action :"showData",
+//             number : RandomNumber
+//         },
+//         dataType: "djson",
+//         success: function (response) {
+//             console.log(reponse);
+//         }
+//     });
+// }
+
+
+// btn.onclick = () => {
+//     fetch("https://api.github.com/users/" + champ.value).then(reponse => reponse.json())
+//     .then(data => {
+//         output.textContent = '$(data.name)';
+        
+//     })
+// }
+
+
+btn.onclick = () =>{
+    fetch("https://api.github.com/users/" + champ.value)
+    .then(reponse => reponse.json())
+    .then(data => {
+        const string = JSON.stringify(data);
+        output.textContent = string;
+        console.table(string);
+    }).catch(error => {
+        console.error("Une erreur s'est produite :", error)});
 }
